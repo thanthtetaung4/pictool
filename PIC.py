42f2fcb9aed1a05a133d6382e036ec07dc23e605
Thant Htet Aung <maung.good@gmail.com>
2025-10-16 14:20:50 +0800

    resize OK

--- PIC.py
+++ PIC.py
@@ -4,30 +4,26 @@ class PIC:
 	flag: str
 	width: int
 	height: int
-	format: str
 
 	def __init__(self, mode, *args):
 		data = args[0]
-		print("creating PIC object")
 		if (mode == -1):
 			raise Exception("Invalid mode")
 		self.mode = mode
 		self.infile = data[0]
 		self.flag = data[1]
 		if (self.mode == 1):
-			self.width = data[2]
-			self.height = data[3]
+			self.width = int(data[2])
+			self.height = int(data[3])
 			self.outfile = data[4]
 		elif (self.mode == 2):
-			self.format = data[2]
 			self.width = 0
 			self.height = 0
-			self.outfile = data[3]
+			self.outfile = data[2]
 
 	def __str__(self):
 		return(f"infile : {self.infile}\n\
 				outfile : {self.outfile}\n\
 				flag : {self.flag}\n\
 				width : {self.width}\n\
-				height : {self.height}\n\
-				format : {self.format}")
+				height : {self.height}\n")
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PIL import Image
 from PIC import PIC
 
 def whichMode(flag: str) -> int:
-    print(flag == "-f")
     if (flag == "-r"):
         return (1)
     elif (flag == "-f"):
@@ -12,20 +11,39 @@ def whichMode(flag: str) -> int:
     return (-1)
 
 def changeExtension(image: PIC):
-    if (os.path.exists(image.infile) and os.path.isdir(image.outfile) and len(image.format) > 0):
-        outfile, e = os.path.splitext(image.infile)
-        outfile += f".{image.format}"
-        if (outfile != image.infile):
+    if (os.path.exists(image.infile)):
+        if (os.path.exists(image.outfile)):
+            print(f"file {image.outfile} already exist")
+            return
+        if (image.outfile != image.infile):
             try:
                 with Image.open(image.infile) as im:
-                    im.save(outfile)
-            except OSError:
-                print("err: cannot conver", image.infile)
+                    im.save(image.outfile)
+                    print(f"new image is saved as {image.outfile}")
+            except OSError as e:
+                print(f"err: {e} cannot convert {image.infile}")
         else:
             print("err: same format as the input aborted")
+    else:
+        print("err: broken path")
 
 def changeSize(image: PIC):
-    pass
+    if (os.path.exists(image.infile)):
+        if (os.path.exists(image.outfile)):
+            print(f"file {image.outfile} already exist")
+            return
+        if (image.outfile != image.infile):
+            try:
+                with Image.open(image.infile) as im:
+                    im = im.resize([image.width, image.height])
+                    im.save(image.outfile)
+                    print(f"new image is saved as {image.outfile}")
+            except OSError as e:
+                print(f"err: {e} cannot convert {image.infile}")
+        else:
+            print("err: same format as the input aborted")
+    else:
+        print("err: broken path")
 
 def printHelp():
     print("Help") # will improve later
@@ -35,23 +53,19 @@ def pic(*argv):
     if (len(data) == 2):
         if (data[1] == "-h" or data[1] == "-help"):
             printHelp()
-    elif (4 >= len(data) <= 5):
-        print(data)
+    elif (5 >= len(data) <= 6):
         mode: int = whichMode(data[1])
         try :
             image = PIC(mode, data)
-            print(image)
             if (image.mode == 1):
-                print("resize")
+                changeSize(image)
             elif (image.mode == 2):
-                print("format")
                 changeExtension(image)
         except Exception as e:
-            print("fatal err:", e)
+            print("fatal err:", e, "aborting")
     else:
-        print("<image_path> -[option] <save_path> <new_format>")
+        print("<image_path> -[option] <new_image_path>")
 
 if __name__ == "__main__":
     del sys.argv[0]
-    print(sys.argv)
     pic(sys.argv)
